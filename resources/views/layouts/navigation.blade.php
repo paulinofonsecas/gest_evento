c84287859881dbeacb7cfe5e5eeaf85e1cce0465
Paulino Fonseca <50420355+paulinofonsecas@users.noreply.github.com>
2023-04-06 11:23:17 +0100

    Começo do fim

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -87,13 +87,9 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-        </div>
     </div>
 </nav>
